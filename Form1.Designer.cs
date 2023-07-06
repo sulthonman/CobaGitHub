a9cf5df6de29e27b02e6f7b0263ca60f482e5494
nandulll <nandafadhilahr@gmail.com>
2023-07-06 20:58:05 +0700

    Nanda

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,9 +47,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(325, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.Size = new System.Drawing.Size(119, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "hon dan Nanda";
+            this.label1.Text = "sulthon dan Nanda";
             // 
             // Form1
             // 
